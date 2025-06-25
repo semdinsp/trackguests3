ab8871f62a0127f56dcda2c5695dd9c25d0e0a1e
Fly Dev <dev@fly.io>
2025-06-25 17:31:08 +0000

    fix SVG syntax error with unmatched path tag

--- lib/trackguests3_web/live/visitor_live/check_in.ex
+++ lib/trackguests3_web/live/visitor_live/check_in.ex
@@ -103,7 +103,7 @@ defmodule Trackguests3Web.VisitorLive.CheckIn do
                     required
                   >
                   <svg class="absolute right-4 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                   </svg>
                 </div>
                 
