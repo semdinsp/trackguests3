9dab1390c86fe1524db6a0d153648d0ec23d5b1d
Fly Dev <dev@fly.io>
2025-06-25 16:34:37 +0000

    add UserAuth import to router

--- lib/trackguests3_web/router.ex
+++ lib/trackguests3_web/router.ex
@@ -3,6 +3,8 @@ defmodule Trackguests3Web.Router do
 
   import Trackguests3Web.UserAuth
 
+  import Trackguests3Web.UserAuth
+
   pipeline :browser do
     plug(:accepts, ["html"])
     plug(:fetch_session)
