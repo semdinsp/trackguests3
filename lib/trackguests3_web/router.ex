828a79ead522958b351f62bd4fc5d8725af9d0aa
Fly Dev <dev@fly.io>
2025-06-25 16:34:31 +0000

    add fetch_current_scope_for_user plug to browser pipeline

--- lib/trackguests3_web/router.ex
+++ lib/trackguests3_web/router.ex
@@ -1,6 +1,8 @@
 defmodule Trackguests3Web.Router do
   use Trackguests3Web, :router
 
+  import Trackguests3Web.UserAuth
+
   pipeline :browser do
     plug(:accepts, ["html"])
     plug(:fetch_session)
@@ -8,6 +10,7 @@ defmodule Trackguests3Web.Router do
     plug(:put_root_layout, html: {Trackguests3Web.Layouts, :root})
     plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
+    plug(:fetch_current_scope_for_user)
   end
 
   pipeline :api do
@@ -44,5 +47,33 @@ defmodule Trackguests3Web.Router do
       live_dashboard("/dashboard", metrics: Trackguests3Web.Telemetry)
       forward("/mailbox", Plug.Swoosh.MailboxPreview)
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", Trackguests3Web do
+    pipe_through([:browser, :require_authenticated_user])
+
+    live_session :require_authenticated_user,
+      on_mount: [{Trackguests3Web.UserAuth, :require_authenticated}] do
+      live("/users/settings", UserLive.Settings, :edit)
+      live("/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email)
+    end
+
+    post("/users/update-password", UserSessionController, :update_password)
+  end
+
+  scope "/", Trackguests3Web do
+    pipe_through([:browser])
+
+    live_session :current_user,
+      on_mount: [{Trackguests3Web.UserAuth, :mount_current_scope}] do
+      live("/users/register", UserLive.Registration, :new)
+      live("/users/log-in", UserLive.Login, :new)
+      live("/users/log-in/:token", UserLive.Confirmation, :new)
+    end
+
+    post("/users/log-in", UserSessionController, :create)
+    delete("/users/log-out", UserSessionController, :delete)
   end
 end
