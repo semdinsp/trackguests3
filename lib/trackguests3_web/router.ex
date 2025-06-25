afd07a8c0f648200ac8fb0c789f28d4bef38e340
Fly Dev <dev@fly.io>
2025-06-25 15:45:31 +0000

    add visitor check-in and check-out routes

--- lib/trackguests3_web/router.ex
+++ lib/trackguests3_web/router.ex
@@ -2,35 +2,38 @@ defmodule Trackguests3Web.Router do
   use Trackguests3Web, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, html: {Trackguests3Web.Layouts, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_live_flash)
+    plug(:put_root_layout, html: {Trackguests3Web.Layouts, :root})
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", Trackguests3Web do
-    pipe_through :browser
+    pipe_through(:browser)
     # NEED ADMIN LEVEL HERE
-    live "/residences", ResidenceLive.Index, :index
-    live "/residences/new", ResidenceLive.Form, :new
-    live "/residences/:id", ResidenceLive.Show, :show
-    live "/residences/:id/edit", ResidenceLive.Form, :edit
+    live("/residences", ResidenceLive.Index, :index)
+    live("/residences/new", ResidenceLive.Form, :new)
+    live("/residences/:id", ResidenceLive.Show, :show)
+    live("/residences/:id/edit", ResidenceLive.Form, :edit)
 
-    live "/rooms", RoomsLive.Index, :index
-    live "/rooms/new", RoomsLive.Form, :new
-    live "/rooms/:id", RoomsLive.Show, :show
-    live "/rooms/:id/edit", RoomsLive.Form, :edit
-
+    live("/rooms", RoomsLive.Index, :index)
+    live("/rooms/new", RoomsLive.Form, :new)
+    live("/rooms/:id", RoomsLive.Show, :show)
+    live("/rooms/:id/edit", RoomsLive.Form, :edit)
 
     # END ADMIN
 
-    get "/", PageController, :home
+    get("/", PageController, :home)
+
+    # Visitor routes (public)
+    live("/visitor/check-in", VisitorLive.CheckIn, :index)
+    live("/visitor/check-out", VisitorLive.CheckOut, :index)
   end
 
   # Other scopes may use custom stacks.
@@ -48,10 +51,10 @@ defmodule Trackguests3Web.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
-      pipe_through :browser
+      pipe_through(:browser)
 
-      live_dashboard "/dashboard", metrics: Trackguests3Web.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+      live_dashboard("/dashboard", metrics: Trackguests3Web.Telemetry)
+      forward("/mailbox", Plug.Swoosh.MailboxPreview)
     end
   end
 end
