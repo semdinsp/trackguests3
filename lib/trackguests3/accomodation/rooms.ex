733e207a5b25758da58d6445bb8b40bcdebfd9c5
Fly Dev <dev@fly.io>
2025-06-29 02:46:01 +0000

    add belongs_to residence association

--- lib/trackguests3/accomodation/rooms.ex
+++ lib/trackguests3/accomodation/rooms.ex
@@ -5,12 +5,13 @@ defmodule Trackguests3.Accomodation.Rooms do
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "rooms" do
-    field :title, :string
-    field :floor, :integer
-    field :needs_fob, :boolean, default: false
-    field :memo, :string
-    field :accepts_guests, :boolean, default: false
-    field :residence_id, :binary_id
+    field(:title, :string)
+    field(:floor, :integer)
+    field(:needs_fob, :boolean, default: false)
+    field(:memo, :string)
+    field(:accepts_guests, :boolean, default: false)
+
+    belongs_to(:residence, Trackguests3.Accomodation.Residence)
 
     timestamps(type: :utc_datetime)
   end
@@ -18,7 +19,7 @@ defmodule Trackguests3.Accomodation.Rooms do
   @doc false
   def changeset(rooms, attrs) do
     rooms
-    |> cast(attrs, [:title, :floor, :needs_fob, :memo, :accepts_guests])
+    |> cast(attrs, [:title, :floor, :needs_fob, :memo, :accepts_guests, :residence_id])
     |> validate_required([:title, :floor, :needs_fob, :memo, :accepts_guests])
   end
 end
