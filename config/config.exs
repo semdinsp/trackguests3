6e5cdf71f0cf61da9a3cae3245bd97f8fa291af1
Scott Sproule <scott.sproule@ficonab.com>
2025-07-28 18:47:59 -0400

    update for logger info env

--- config/config.exs
+++ config/config.exs
@@ -75,4 +75,6 @@ config :phoenix, :json_library, Jason
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
+require Logger
+Logger.info "Scott: Loading configuration for environment: #{config_env()}"
 import_config "#{config_env()}.exs"
