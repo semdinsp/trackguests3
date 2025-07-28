0124a31ef5ef98876a3732d08eff1f5795afdd9c
Scott Sproule <scott.sproule@ficonab.com>
2025-07-28 13:08:30 -0400

    update for deploy issue

--- priv/repo/migrations/20250625163410_create_users_auth_tables.exs
+++ priv/repo/migrations/20250625163410_create_users_auth_tables.exs
@@ -4,7 +4,7 @@ defmodule Trackguests3.Repo.Migrations.CreateUsersAuthTables do
   def change do
     create table(:users, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :email, :string, null: false, collate: :nocase
+      add :email, :string, null: false
       add :hashed_password, :string
       add :confirmed_at, :utc_datetime
 
@@ -16,7 +16,7 @@ defmodule Trackguests3.Repo.Migrations.CreateUsersAuthTables do
     create table(:users_tokens, primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :user_id, references(:users, type: :binary_id, on_delete: :delete_all), null: false
-      add :token, :binary, null: false, size: 32
+      add :token, :binary, null: false
       add :context, :string, null: false
       add :sent_to, :string
       add :authenticated_at, :utc_datetime
