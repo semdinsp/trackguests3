a1a740922b206b048899bec327792e66477b55cc
Scott Sproule <scott.sproule@ficonab.com>
2025-07-28 12:48:28 -0400

    update for repo

--- lib/trackguests3/repo.ex
+++ lib/trackguests3/repo.ex
@@ -1,5 +1,9 @@
 defmodule Trackguests3.Repo do
   use Ecto.Repo,
     otp_app: :trackguests3,
-    adapter: Ecto.Adapters.SQLite3
+    #adapter: Ecto.Adapters.SQLite3
+    adapter: if(Mix.env() in [:dev, :test],
+        do: Ecto.Adapters.SQLite3,
+        else: Ecto.Adapters.Postgres
+      )
 end
--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule Trackguests3.MixProject do
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
       {:ecto_sqlite3, ">= 0.0.0",  only: [:dev,:test]},
-      {:postgrex, ">= 0.0.0"},
+      {:postgrex, ">= 0.0.0", only: :prod},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.0.0"},
